2c5279f1652853cafe7559e806fcfd1b64002438
Long <darknesszx@gmail.com>
2017-07-27 11:41:22 +0700

    init

---
+++ src/components/common/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const Button = () => {
+  return (
+    <TouchableOpacity>
+      <Text></Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = {
+  buttonStyle : {
+    borderWidth : 1,
+    borderColor : "#007aff",
+    borderRadius : 10
+  }
+}
+
+export default Button;
